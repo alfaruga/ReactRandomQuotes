18f32ac60fd068fc76505d7810a001ae1148bf8e
Alexis Ruiz <alfaruga@gmail.com>
2020-09-26 15:40:21 -0500

    Removed some firebase extra code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import classes from './App.module.css';
 import QuoteBox from "./containers/QuoteBox/QuoteBox";
 import Modal from "./components/UI/Modal/Modal";
-import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faChevronRight, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,5 +1,4 @@
 import React from "react";
-import firebase from "firebase";
 import classes from "./Modal.module.css";
 
 const modal = () => <div className={classes.Modal}></div>
