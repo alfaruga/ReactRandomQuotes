7587dd817ced5d26be7efa5b23bcb2e5e0a2018e
Alexis Ruiz <alfaruga@gmail.com>
2020-09-26 09:21:12 -0500

    Updates

---
+++ precache-manifest.b0dea1dcd79677b10e71f72b092e2d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684b5a46b9e2e970ec1162aaeee84a1a",
+    "url": "/ReactRandomQuotes/index.html"
+  },
+  {
+    "revision": "77272e34706447867852",
+    "url": "/ReactRandomQuotes/static/css/main.5cd383c6.chunk.css"
+  },
+  {
+    "revision": "61a7b7800c9c001b5e40",
+    "url": "/ReactRandomQuotes/static/js/2.46f46981.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/ReactRandomQuotes/static/js/2.46f46981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77272e34706447867852",
+    "url": "/ReactRandomQuotes/static/js/main.c58bae3a.chunk.js"
+  },
+  {
+    "revision": "58b3fe3fe606593ec23c",
+    "url": "/ReactRandomQuotes/static/js/runtime-main.d36fa834.js"
+  }
+]);
